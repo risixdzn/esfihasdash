d557e15e6b22646844aad6f41b1757438c64313a
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-03-20 17:37:25 -0300

    (cssfix) fix dashcontent locked overflow

--- src/components/sidebar/pages/home/home.jsx
+++ src/components/sidebar/pages/home/home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Welcome from '../components/Welcome/welcome'
+import './home.css'
 
 function Home () {    
   return (
     <div>
         <Welcome action="fez" data="pedido(s)." create='Novo pedido' type='pedidos'/>
-        <h1 style={{fontSize: 50, fontWeight: 600, color: "white"}}>Home</h1>        
+        <h1 style={{fontSize: 50, fontWeight: 600, color: "white"}}>Home</h1>      
+        <div className="bigdiv">
+          <h1>Ainda não há nada aqui.</h1>  
+        </div>        
     </div>
   )
 }
--- src/components/sidebar/pages/pessoas.jsx
+++ src/components/sidebar/pages/pessoas.jsx
@@ -6,6 +6,7 @@ const Pessoas = () => {
     <div>
         <Welcome action="cadastrou" data="pessoa(s)." create='Nova pessoa' type='pessoas'/>
         <h1 style={{fontSize: 50, fontWeight: 600, color: "white"}}>Pessoas</h1>
+        
     </div>
   )
 }
