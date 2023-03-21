87c831f3872b58e918ecb51ed544e6aeb77e6d1e
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-03-20 22:13:54 -0300

    (feature) add register client page + form

---
+++ src/components/sidebar/pages/pessoas/pessoasList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Welcome from '../components/Welcome/welcome'
+
+const PessoasList = () => {
+  return (
+    <div>
+        <Welcome action="cadastrou" data="pessoa(s)." create='Nova pessoa' type='pessoas'/>
+        <h1 style={{fontSize: 50, fontWeight: 600, color: "white"}}>Pessoas</h1>         
+    </div>
+  )
+}
+
+export default PessoasList
