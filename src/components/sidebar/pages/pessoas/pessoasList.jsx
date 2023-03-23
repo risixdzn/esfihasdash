bef420d8373c770df5359fa91abbcafc6b730f92
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-03-23 18:40:33 -0300

    (feature) add deletemodal + show pessoas

--- src/components/sidebar/pages/pessoas/pessoasList.jsx
+++ src/components/sidebar/pages/pessoas/pessoasList.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Welcome from '../components/Welcome/welcome'
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import DisplayPessoas from '../../../../db/DisplayPessoas/DisplayPessoas';
+import ModalDeletePessoas from './modals/modalDeletePessoa';
+
+import { useState } from 'react';
 
 const PessoasList = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [deletingPessoa, setDeletingPessoa] = useState();
+
   return (
-    <div>
+    <div style={showModal? {Y:"hidden"}:{Y:"scroll"}}>
+        <ModalDeletePessoas show={showModal} setShowModal={setShowModal} deletingPessoa={deletingPessoa}/>
         <Welcome action="cadastrou" data="pessoa(s)." create='Nova pessoa' type='pessoas'/>          
-        {/* <DisplayPessoas/> */}
-        <ToastContainer theme='dark' limit='2'/>     
+        <DisplayPessoas setShowModal={setShowModal} setDeletingPessoa={setDeletingPessoa}/>
+        <ToastContainer theme='dark' limit='2'/>          
     </div>
   )
 }
--- src/db/DisplayPessoas/DisplayPessoas.jsx
+++ src/db/DisplayPessoas/DisplayPessoas.jsx
@@ -1,14 +1,22 @@
-import React, { useLayoutEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { db } from "../../firebase-config";
 import { UserAuth } from "../../context/AuthContext";
 import { getDocs, collection } from "firebase/firestore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 import './displaypessoas.css'
 
-function DisplayPessoas() {
+function DisplayPessoas({setShowModal, setDeletingPessoa, deletingPessoa}) {
     const [loading, setIsloading] = useState(true);
     const [showPessoas, setShowPessoas] = useState([]);
     const { user } = UserAuth();
+
+    const handleDelete = (key) => {
+        setDeletingPessoa(key);  
+        console.log(key);
+        setShowModal(true);        
+    };
 
     const hasLoaded = useRef(false); // cria uma referência a um booleano que será atualizado após o carregamento da página   
 
@@ -32,7 +40,7 @@ function DisplayPessoas() {
             //console.log(showPessoas);
         }
     }, [user.uid]);
-
+   
     if (loading){
         return (
             <div className="pessoascontainer" style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
@@ -40,14 +48,15 @@ function DisplayPessoas() {
             </div>
         )
     }
+ 
 
-    return(
+    return(        
         <div className="pessoascontainer">
             <h1 className="title">Pessoas</h1>
             <div className="pessoasgrid">
                 {showPessoas.length !== 0 ? showPessoas.map((pessoa)=>{
                     return (
-                        <div className="pessoa">
+                        <div className="pessoa" key={pessoa.key} id={pessoa.key}>
                             <div className="picwrapper">
                                 <div className="pic">
                                     <img src={pessoa.foto !== "" ? pessoa.foto : "../assets/img/user.png" } alt="pic"></img>
@@ -55,12 +64,12 @@ function DisplayPessoas() {
                             </div>
                             <div className="info">
                                 <h1 className="name" id={pessoa.key} key={pessoa.key}>{pessoa.key}</h1>
+                                <button><FontAwesomeIcon icon={faTrashCan} className="delbtn" key={pessoa.key} onClick={() => handleDelete(pessoa.key)}/></button>
                             </div>                            
                         </div>
                     ) 
                 }):<img className="loader" src="../assets/gif/rippleloader.svg" alt="loading" />}
-            </div>
-            
+            </div>            
         </div>
     ) 
 }
