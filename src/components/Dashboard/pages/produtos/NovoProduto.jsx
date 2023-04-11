8cfb44816603f55bb507607a549229397f791f91
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-04-11 18:49:03 -0300

    :crystal_ball: (feature) add new product

--- src/components/Dashboard/pages/produtos/NovoProduto.jsx
+++ src/components/Dashboard/pages/produtos/NovoProduto.jsx
@@ -1,6 +1,6 @@
 import React, {  useState, useEffect } from 'react'
 import '../css/NovoItem.css'
-import { faUser, faImage } from '@fortawesome/free-solid-svg-icons';
+import { faPizzaSlice, faImage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -11,67 +11,67 @@ import { doc, setDoc } from 'firebase/firestore';
 
 import {  toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
-function NovaPessoa() {
 
+function NovoProduto() {
   const { user } = UserAuth();
 
   const [ isLoading, setIsLoading ] = useState(false);
-  const [ pessoaName, setPessoaName] = useState("");
-  const [ pessoaPic, setPessoaPic] = useState("");
+  const [ produtoName, setProdutoName] = useState("");
+  const [ produtoPic, setProdutoPic] = useState("");
 
   const navigate = useNavigate();
 
-  async function criarPessoa(e) {
+  async function criarProduto(e) {
     e.preventDefault();
     setIsLoading(true);
     
-    //criapessoa
-    await setDoc(doc(db, "users", user.uid, "pessoas", pessoaName),{
+    //criaproduto
+    await setDoc(doc(db, "users", user.uid, "produtos", produtoName),{
       pedidos: 0,
-      foto: pessoaPic,   
-      nome: pessoaName,   
+      foto: produtoPic,   
+      nome: produtoName,   
     });  
-    navigate("/pessoas/list");                
+    navigate("/produtos/list");                
     // await new Promise(resolve => setTimeout(resolve, 1000)); // aguarda 1 segundo para a página recarregar completamente       
     setIsLoading(false);    
-    localStorage.setItem("createdPessoa", pessoaName); // Armazena o nome da pessoa deletada no Local Storage 
+    localStorage.setItem("createdProduto", produtoName); // Armazena o nome da produto deletada no Local Storage 
     window.location.reload();
   }
 
   useEffect(()=>{
-    const createdPessoa = localStorage.getItem("createdPessoa");//puxa o item do localstorage
-    if (createdPessoa) {
-        toast.success("Pessoa " + createdPessoa + " criada.");
-        localStorage.removeItem("createdPessoa"); // Remove a informação da notificação do Local Storage
+    const createdProduto = localStorage.getItem("createdProduto");//puxa o item do localstorage
+    if (createdProduto) {
+        toast.success("Produto " + createdProduto + " criado.");
+        localStorage.removeItem("createdProduto"); // Remove a informação da notificação do Local Storage
     }
   },[user.uid])    
 
   return (
     <div className='wrapper'>      
-      <form className='novoitem' onSubmit={criarPessoa}>
-        <h1 className='title'>Nova pessoa</h1>
+      <form className='novoitem' onSubmit={criarProduto}>
+        <h1 className='title'>Novo produto</h1>
         <div className='inputcontainer'>              
-          <input required={true} disabled={isLoading? true : false} type="text" id="name" name="Name" placeholder='Nome *' autoFocus={false}
+          <input required={true} disabled={isLoading? true : false} type="text" id="name" name="Name" placeholder='Nome do produto (*)' autoFocus={false}
             onChange={(event) =>{
-            setPessoaName(event.target.value)}}>    
+            setProdutoName(event.target.value)}}>    
           </input>
-          <label className='control-label' htmlFor="email"><FontAwesomeIcon icon={faUser}/></label>
+          <label className='control-label' htmlFor="email"><FontAwesomeIcon icon={faPizzaSlice}/></label>
         </div>     
 
         <div className='inputcontainer'>              
           <input required={false} disabled={isLoading? true : false} type="url" id="Foto" name="Foto" placeholder='URL da foto (opcional)' autoFocus={false}
             onChange={(event) =>{
-            setPessoaPic(event.target.value)}}>    
+            setProdutoPic(event.target.value)}}>    
           </input>
           <label className='control-label' htmlFor="password"><FontAwesomeIcon icon={faImage}/></label>
          </div>    
 
         <button className="entrarBtn" type='submit'>{isLoading ? <img src='../assets/gif/rippleloader.svg' style={{height:"75%"}}alt=''></img> : "Cadastrar"}</button>   
 
-        <p><Link to='/pessoas/list' className='linkbtn'>Voltar</Link></p>
+        <p><Link to='/produtos/list' className='linkbtn'>Voltar</Link></p>
       </form>      
     </div>
   )
 }
 
-export default NovaPessoa
+export default NovoProduto
