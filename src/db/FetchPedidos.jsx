cd6148b46114bf7baccf823d64d86050c35ed0d9
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-04-10 22:14:31 -0300

    :crystal_ball: (feature) skeleton component

--- src/db/FetchPedidos.jsx
+++ src/db/FetchPedidos.jsx
@@ -3,19 +3,26 @@ import { collection } from "firebase/firestore";
 import { getCountFromServer } from "firebase/firestore";//import your function for getting count
 import { db } from "../firebase-config";
 
+import Skeleton from "../components/Dashboard/pages/components/Skeleton/skeleton";
+
 function PedidosCount({ user }) {
   const [pedidos, setPedidos] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     async function fetchPedidos() {
       const pedidosRef = collection(db, "users", user.uid, "pedidos");
       const snapshot = await getCountFromServer(pedidosRef);
       setPedidos(snapshot.data().count - 1);
+      setLoading(false);
     }
     fetchPedidos();
   }, [user.uid]);
 
-  return <span>{pedidos}</span>;
+  return (
+    (loading? <Skeleton width="15px" height="20px"/> : <span>{pedidos}</span>)
+  )
 }
 
 export default PedidosCount;
--- src/db/FetchPessoas.jsx
+++ src/db/FetchPessoas.jsx
@@ -3,34 +3,33 @@ import { collection } from "firebase/firestore";
 import { getCountFromServer } from "firebase/firestore";
 import { db } from "../firebase-config";
 
+import Skeleton from "../components/Dashboard/pages/components/Skeleton/skeleton";
+
 function PessoasCount({ user }) {
   const [pessoas, setPessoas] = useState(0);
+  const [loading, setLoading] = useState(false);
  
   useEffect(() => {    
+    setLoading(true)
     async function fetchPessoas() {
       const pessoasRef = collection(db, "users", user.uid, "pessoas");
       const snapshot = await getCountFromServer(pessoasRef);
       setPessoas(snapshot.data().count);
+      setLoading(false)
     }
-    fetchPessoas();    
-  }, [user.uid]);
-
-
-  async function updatePessoasCount(user) {   
-    const pessoasRef = collection(db, "users", user.uid, "pessoas", "count");
-    const snapshot = await getCountFromServer(pessoasRef);
-    setPessoas(snapshot.data().count);      
-  } 
+    fetchPessoas();        
+  }, [user.uid]);  
 
   return (
-    <span>{pessoas}</span>
-  );
+    (loading? <Skeleton width="15px" height="20px"/> : <span>{pessoas}</span>)
+  )
 }
 
 async function updatePessoasCount(user, setPessoas) {
   const pessoasRef = collection(db, "users", user.uid, "pessoas", "count");
   const snapshot = await getCountFromServer(pessoasRef);
   setPessoas(snapshot.data().count);    
+  alert("a");
 } 
 
 export { updatePessoasCount };
