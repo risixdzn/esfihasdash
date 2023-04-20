72705b586b5143940d570310c2c08e48d4cc1760
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-04-20 19:41:04 -0300

    :crystal_ball: (feature) Pedido - Update SomaPedidosIguais logic

--- src/db/somaPedidosIguais.jsx
+++ src/db/somaPedidosIguais.jsx
@@ -1,39 +1,53 @@
-const pedido = [
-    {
-        nome: "Ricardo",
-        itens: {
-            calabresa: 2,
-            carne: 3,
-            queijo: 1,
-        }
-    },
-    {
-        nome: "Joao",
-        itens: {
-            brigadeiro: 1,
-            carne: 2,
-            queijo: 3,
-            calabresa: 6,
-        }
-    }
-]
+const clientes = {
+  "Ricardo": {
+      "nome": "Ricardo",
+      "foto": "https://classic.exame.com/wp-content/uploads/2016/09/size_960_16_9_planeta-dos-macacos-divulgacao.jpg?quality=70&strip=info&w=960",
+      "itens": {
+          "Frango": {
+              "nomeproduto": "Frango",
+              "quantidade": "1",
+              "foto": "https://raw.githubusercontent.com/risixdzn/esfihasdash/test/public/assets/img/esfihas/frango.png"
+          },
+          "Carne": {
+              "nomeproduto": "Carne",
+              "quantidade": "1",
+              "foto": "https://raw.githubusercontent.com/risixdzn/esfihasdash/test/public/assets/img/esfihas/carne.png"
+          }
+      }
+  },
+  "Rosete": {
+      "nome": "Rosete",
+      "foto": "https://st.depositphotos.com/1004998/1907/i/600/depositphotos_19073037-stock-photo-daisy-flower.jpg",
+      "itens": {
+          "Frango": {
+              "nomeproduto": "Frango",
+              "quantidade": "2",
+              "foto": "https://raw.githubusercontent.com/risixdzn/esfihasdash/test/public/assets/img/esfihas/frango.png"
+          },
+          "Napolitana": {
+              "nomeproduto": "Napolitana",
+              "quantidade": "1",
+              "foto": ""
+          }
+      }
+  }
+};
 
-// Criamos um objeto vazio para armazenar as quantidades somadas
-const somas = {};
+let totalItens = {};
 
-// Iteramos sobre cada elemento do array de pedidos
-pedido.forEach((pessoa) => {
-  // Iteramos sobre cada item do pedido da pessoa atual
-  Object.entries(pessoa.itens).forEach(([sabor, quantidade]) => {
-    // Verificamos se já existe uma entrada no objeto somas para o sabor em questão
-    if (!somas[sabor]) {
-      // Se não existe, criamos uma nova entrada com a quantidade atual
-      somas[sabor] = quantidade;
-    } else {
-      // Se já existe, somamos a quantidade atual com a que já existe no objeto
-      somas[sabor] += quantidade;
-    }
+// Percorre todos os clientes
+Object.values(clientes).forEach(cliente => {
+  // Percorre todos os itens do cliente
+  Object.values(cliente.itens).forEach(item => {
+      // Verifica se o item já existe no objeto de totalItens
+      if (totalItens[item.nomeproduto]) {
+          // Adiciona a quantidade do item existente com a quantidade atual do item
+          totalItens[item.nomeproduto] += parseInt(item.quantidade);
+      } else {
+          // Adiciona o item ao objeto de totalItens com sua quantidade
+          totalItens[item.nomeproduto] = parseInt(item.quantidade);
+      }
   });
 });
 
-console.log(somas);
+console.log(totalItens);
