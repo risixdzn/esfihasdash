2947586d12f5363a6f027aa2f0748f53679ae0f1
Ricardo Amorim <102877738+risixdzn@users.noreply.github.com>
2023-04-20 23:47:40 -0300

    :dart: (bugfix) Pedido - Broken addproduto

--- src/components/Dashboard/pages/pedidos/new/SelectProdutos/functions/HandleProdutoChange.jsx
+++ src/components/Dashboard/pages/pedidos/new/SelectProdutos/functions/HandleProdutoChange.jsx
@@ -1,5 +1,5 @@
 function HandleProdutoChange(event, showProdutos, setProdutoSelecionado, setImgProdutoSelecionado) {        
-    const selectProduto = event.value        
+    const selectProduto = event.target.value        
     setProdutoSelecionado(selectProduto);
     // encontrar o objeto com o nome igual a selectProduto
     const produto = showProdutos.find(p => p.nome === selectProduto);        
